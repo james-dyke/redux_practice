18a84c4073a9ef1679428ab9c8134e8462701baf
James Dyke <jcdyke@gmail.com>
2022-12-09 15:41:10 -0600

    Overscroll behaviour y contained

--- store/actions.js
+++ store/actions.js
@@ -16,10 +16,11 @@ export const getCharacters = (axios) => (dispatch, getState) => {
   //   if (state.quoteOfTheDay.quote) {
   //     return;
   //   }
-  dispatch(setLoadingState({ loading: true }));
+
   axios
     .get("https://rickandmortyapi.com/api/character")
     .then(({ data }) => {
+      dispatch(setLoadingState({ loading: true }));
       const res = data.results;
       const nextPage = data.info.next;
       const urlParams = new URLSearchParams(nextPage);
